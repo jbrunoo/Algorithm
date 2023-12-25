3702ddf29d7b3728c2302ff508234569380316e6
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2023-12-25 19:19:24 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.kt
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.kt
@@ -1,8 +1,8 @@
 class Solution {
     fun solution(a: Int, b: Int): Int {
         var answer: Int = 0
-        val sum_ab = (a.toString() + b.toString()).toInt()
-        val sum_ba = (b.toString() + a.toString()).toInt()
+        val sum_ab = StringBuilder().append(a).append(b).toString().toInt()
+        val sum_ba = StringBuilder().append(b).append(a).toString().toInt()
         answer = if(sum_ab <= sum_ba) sum_ba else sum_ab
         return answer
         
