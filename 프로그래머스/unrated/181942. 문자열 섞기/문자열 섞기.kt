81acb58ee4b0cbd1bc05c83c96cb782411763568
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2023-12-25 18:31:59 +0900

    [level unrated] Title: 문자열 섞기, Time: 27.46 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(str1: String, str2: String): String {
+        var answer: String = ""
+        (str1.indices).forEach {
+                answer = answer + str1[it] + str2[it]
+            }
+        return answer
+    }
+}
