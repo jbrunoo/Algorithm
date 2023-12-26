b219f3f69a8d573575f7ed83fd715d589244c9d8
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2023-12-26 19:00:13 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun solution(code: String): String {
+        var answer: String = ""
+        var mode: Int = 0
+        
+        for(idx in 0 until code.length) {
+            if(mode == 0) {
+                if(code[idx] != '1') {
+                    if(idx % 2 == 0) answer += code[idx]
+                }
+                else mode = 1
+            } else {
+                if(code[idx] != '1') {
+                    if(idx % 2 != 0) answer += code[idx]
+                }
+                else mode = 0
+            }
+        }
+        
+        if(answer.isEmpty()) answer = "EMPTY"
+        
+        return answer
+    }
+}
