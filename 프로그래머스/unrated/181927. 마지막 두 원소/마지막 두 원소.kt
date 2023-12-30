a9b68d20449cc11a1466da107e8efaea00b373e2
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2023-12-30 22:56:55 +0900

    [level unrated] Title: 마지막 두 원소, Time: 5.81 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.kt
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.kt
@@ -1,11 +1,14 @@
 class Solution {
     fun solution(num_list: IntArray): IntArray {
         var answer: MutableList<Int> = mutableListOf()
-        val size = num_list.size
-        for(i in num_list) {
-            answer.add(i)
+        val first = num_list[num_list.size - 2]
+        val second = num_list[num_list.size - 1]
+        
+        for(num in num_list) {
+            answer.add(num)
         }
-        if(num_list[size - 2] < num_list[size - 1]) answer.add(num_list[size - 1] - num_list[size - 2]) else answer.add(num_list[size - 1] * 2)
+        
+        if(first < second) answer.add(second - first) else answer.add(second * 2)
         return answer.toIntArray()
     }
 }
