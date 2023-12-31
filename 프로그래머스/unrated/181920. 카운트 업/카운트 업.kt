5498198484febc256a2b975190c9d692d6fb459b
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-01 00:39:34 +0900

    [level unrated] Title: 카운트 업, Time: 7.42 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.kt
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.kt
@@ -1,9 +1,5 @@
 class Solution {
     fun solution(start_num: Int, end_num: Int): IntArray {
-        var answer: MutableList<Int> = mutableListOf()
-        (start_num..end_num).forEach {
-            answer.add(it)
-        }
-        return answer.toIntArray()
+        return (start_num..end_num).toList().toIntArray()    
     }
 }
