7b8d3c052a305603f97a4e76cc254b7d18a3959f
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-01 16:22:55 +0900

    [level unrated] Title: 배열 만들기 4, Time: 9.65 ms, Memory: 63.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        var stk: MutableList<Int> = mutableListOf()
+        var i = 0
+        while(i < arr.size) {
+            if(stk.isEmpty() || stk.last() < arr[i]) {
+                stk.add(arr[i]) 
+                i++
+            }
+            else stk.removeAt(stk.size - 1)
+        }
+        return stk.toIntArray()
+    }
+}
