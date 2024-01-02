6af0fc3a37021c90cb9153586b1e8facc7b62380
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-02 12:33:49 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 6.08 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.kt
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.kt
@@ -1,6 +1,7 @@
 class Solution {
     fun solution(my_string: String, is_suffix: String): Int {
-        val suf = my_string.indices.map { my_string.substring(it) }
-            return if(suf.any { it == is_suffix }) 1 else 0
+        return if(my_string.endsWith(is_suffix)) 1 else 0
+        // val suf = my_string.indices.map { my_string.substring(it) }
+        //     return if(suf.any { it == is_suffix }) 1 else 0
     }
 }
