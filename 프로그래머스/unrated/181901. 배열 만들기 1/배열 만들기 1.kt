64a8ea0b3db4fa02a46f2416ee237bd7540ace60
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-03 00:52:14 +0900

    [level unrated] Title: 배열 만들기 1, Time: 8.78 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.kt
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.kt
@@ -1,9 +1,3 @@
 class Solution {
-    fun solution(n: Int, k: Int): IntArray {
-        var answer: IntArray = intArrayOf()
-        for(i in 1..n) {
-            if(i % k == 0) answer += i
-        }
-        return answer
-    }
+    fun solution(n: Int, k: Int) = (k..n step k).toList()
 }
