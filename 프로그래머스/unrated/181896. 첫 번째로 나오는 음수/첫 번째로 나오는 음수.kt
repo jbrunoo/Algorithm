0bf28ed481b95d3ecb17887edad3bbb6db9c2c05
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-04 01:01:19 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 15.01 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.kt
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.kt
@@ -1,5 +1,6 @@
 class Solution {
     fun solution(num_list: IntArray): Int {
             return num_list.indices.firstOrNull { num_list[it] < 0 } ?: -1
+        // indexOfFirst는 null 대신 -1 
     }
 }
