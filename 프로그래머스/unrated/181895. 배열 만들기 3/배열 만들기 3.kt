d25ca1e9bb6c307ef902d1e249c1acafa48754dd
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-04 01:11:49 +0900

    [level unrated] Title: 배열 만들기 3, Time: 24.32 ms, Memory: 64.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(arr: IntArray, intervals: Array<IntArray>): IntArray {
+        val (a, b) = intervals.first()
+        val (c, d) = intervals.last()
+        return (arr.slice(a..b) + arr.slice(c..d)).toIntArray()
+    }
+}
