3b8404278ea089b3405ff9146cd608c29e150d94
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-04 00:54:28 +0900

    [level unrated] Title: 리스트 자르기, Time: 29.06 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.kt
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.kt
@@ -1,12 +1,12 @@
 class Solution {
-    fun solution(n: Int, slicer: IntArray, num_list: IntArray): List<Int> {
+    fun solution(n: Int, slicer: IntArray, num_list: IntArray): IntArray {
         val (a, b, c) = slicer
         return when(n) {
-            1 -> num_list.slice(0..b).toList()
-            2 -> num_list.slice(a until num_list.size).toList()
-            3 -> num_list.slice(a..b).toList()
+            1 -> num_list.slice(0..b)
+            2 -> num_list.slice(a until num_list.size)
+            3 -> num_list.slice(a..b)
             4 -> num_list.slice(a..b step c)
             else -> throw IllegalArgumentException("Invalid value")
-        }
+        }.toIntArray() // 반환값을 List<Int>일 때, slice가 List<Int>를 반환하니 명시안했는데 .toList()를 쓰지않아서 4번 경우 말고 실행값이 undefined 오류가 남. 원인은 잘 모르겠으나 when절 안에 있어서 그런지 ? 명시적으로 써줘야 할듯 함..
     }
 }
