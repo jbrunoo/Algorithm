17bf7abf0c5757ce03347e77dbfbc0fa8e7cce4b
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-05 01:12:13 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 8.54 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.kt
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.kt
@@ -1,7 +1,10 @@
 class Solution {
     fun solution(num_list: IntArray): Int {
-        val odd = (num_list.indices step 2).sumOf { num_list[it] }
-        val even = (1 until num_list.size step 2).sumOf { num_list[it] }
+        val odd = num_list.filterIndexed {i, _ -> i % 2 == 0 }.sum()
+        val even = num_list.filterIndexed {i, _ -> i % 2 != 0 }.sum()
+        
+        // val odd = (num_list.indices step 2).sumOf { num_list[it] }
+        // val even = (1 until num_list.size step 2).sumOf { num_list[it] }
         return if(odd > even) odd else even 
     }
 }
