a9690663c000c92286f6af5cff67261eab869cf3
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-06 01:57:34 +0900

    [level unrated] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.07 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.kt
+++ 프로그래머스/unrated/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.kt
@@ -1,8 +1,13 @@
 class Solution {
     fun solution(strArr: Array<String>): Array<String> {
-        return strArr.indices.map {
-            if(it % 2 == 0) strArr[it].lowercase()
-            else strArr[it].uppercase()
+        // return strArr.indices.map {
+        //     if(it % 2 == 0) strArr[it].lowercase()
+        //     else strArr[it].uppercase()
+        // }.toTypedArray()
+        // mapIndexed 가 훨씬 빠름
+        return strArr.mapIndexed { i, v ->
+            if(i % 2 == 0) v.lowercase()
+            else v.uppercase()
         }.toTypedArray()
     }
 }
