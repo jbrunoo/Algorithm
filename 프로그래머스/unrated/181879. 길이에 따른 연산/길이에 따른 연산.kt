ce8adb0fad464b2e1925ed3e260b9f171a2c1313
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-06 01:47:00 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.kt
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.kt
@@ -1,5 +1,6 @@
 class Solution {
     fun solution(num_list: IntArray): Int {
-        return if(num_list.size >= 11) num_list.sum() else num_list.fold(1) { acc, n -> acc * n }
+        // 합도 sum()보다 fold, reduce로 쓰는게 속도가 빠름.
+        return if(num_list.size >= 11) num_list.fold(0) { acc, n -> acc + n} else num_list.fold(1) { acc, n -> acc * n }
     }
 }
