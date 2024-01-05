c90a46914e2780c9f4a3a7eb872ec010d5311407
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-05 22:40:57 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.04 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
@@ -1,5 +1,8 @@
 class Solution {
     fun solution(a: Int, b: Int): Long {
-        return if(a <= b) (a.toLong()..b).sum() else (b.toLong()..a).sum()
+        // return if(a <= b) (a.toLong()..b).sum() else (b.toLong()..a).sum()
+        // a+b * a..b길이 / 2 로 속도 단축. double도 8byte
+        return if(a <= b) ((a+b) * (b - a + 1).toDouble() / 2).toLong() else ((a+b) * (a - b + 1).toDouble() / 2).toLong()
+        
     }
 }
