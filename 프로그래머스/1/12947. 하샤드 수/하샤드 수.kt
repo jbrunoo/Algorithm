9fa9a793b444369b7a1dd60a4b450531066e9624
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-05 16:00:07 +0900

    [level 1] Title: 하샤드 수, Time: 6.08 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun solution(x: Int): Boolean {
-        val sum = x.toString().map { it.digitToInt() }.sum()
-        println(sum)
+        val sum = x.toString().map { it - '0' }.sum() // sum & fold
+        // it:char형 .toString().toInt() || .digitToInt() || -'0'
         return x % sum == 0 
     }
 }
