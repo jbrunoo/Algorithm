1a816b6902d9f5681122aa9ebad75fb6328a7b9a
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-07 01:50:42 +0900

    [level unrated] Title: 간단한 식 계산하기, Time: 6.51 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.kt
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.kt
@@ -1,18 +1,10 @@
 class Solution {
     fun solution(binomial: String): Int {
-        // val op = listOf('+', '-', '*')
-        val s = binomial.replace(" ", "")
-        return if('+' in s) {
-            val li = s.split('+').map { it.toInt() }
-            li[0] + li[1]
-        }
-        else if ('-' in s) {
-            val li = s.split('-').map { it.toInt() }
-            li[0] - li[1]
-        }
-        else {
-            val li = s.split('*').map { it.toInt() }
-            li[0] * li[1]
+        val (a, op, b) = binomial.split(" ")
+        return when(op) {
+            "+" -> a.toInt() + b.toInt()
+            "-" -> a.toInt() - b.toInt()
+            else -> a.toInt() * b.toInt()
         }
     }
 }
