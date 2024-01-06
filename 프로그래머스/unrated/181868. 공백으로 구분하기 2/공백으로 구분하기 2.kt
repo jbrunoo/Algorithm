f30ae399e23aaa7bc9ac8b2d5fdfae4a7f377375
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-07 01:26:06 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 13.09 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
@@ -1,18 +1,19 @@
 class Solution {
     fun solution(my_string: String): Array<String> {
-        var s = StringBuilder()
-        var answer = arrayOf<String>()
-        for(i in my_string.trim()) {
-            if(i != ' ') s.append(i)
-            else {
-                if(s.isNotEmpty()) {
-                    answer += s.toString()
-                    s.clear()
-                }
-                else continue
-            }
-        }
-        answer += s.toString()
-        return answer
+        return my_string.split(" ").filter { it.isNotBlank() }.toTypedArray()
+        // var s = StringBuilder()
+        // var answer = arrayOf<String>()
+        // for(i in my_string.trim()) {
+        //     if(i != ' ') s.append(i)
+        //     else {
+        //         if(s.isNotEmpty()) {
+        //             answer += s.toString()
+        //             s.clear()
+        //         }
+        //         else continue
+        //     }
+        // }
+        // answer += s.toString()
+        // return answer
     }
 }
