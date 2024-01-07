5f1a0008f0f2473efa268b8d576f19ffa697012a
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-08 00:52:28 +0900

    [level unrated] Title: 배열의 원소만큼 추가하기, Time: 5.69 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.kt
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.kt
@@ -1,11 +1,13 @@
 class Solution {
     fun solution(arr: IntArray): IntArray {
-        var answer: IntArray = intArrayOf()
-        arr.forEach {n ->
-            (1..n).forEach {
-                answer += n
-            }
-        }
-        return answer
+        // var answer: IntArray = intArrayOf()
+        // arr.forEach {n ->
+        //     (1..n).forEach {
+        //         answer += n
+        //     }
+        // }
+        // return answer
+        
+        return arr.flatMap { i -> List(i) { i } }.toIntArray()
     }
 }
