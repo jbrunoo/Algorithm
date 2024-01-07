431d55c11619ae5b1d9e2319415f937a67b6d560
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-08 00:43:21 +0900

    [level unrated] Title: 세 개의 구분자, Time: 73.35 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.kt
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.kt
@@ -3,10 +3,11 @@ class Solution {
         var answer: Array<String> = arrayOf<String>()
         // var s = StringBuilder(myStr)
         var s = myStr
-        s = s.replace("a", " ")
-        s = s.replace("b", " ")
-        s = s.replace("c", " ")
-        answer = s.trim().split(" ").filter { it.length > 0 }.toTypedArray()
+        s = s.replace(Regex("a|b|c"), " ") // .toRegex()
+        // s = s.replace("b", " ")
+        // s = s.replace("c", " ")
+        answer = s.trim().split(" ").filter { it.length > 0 }.toTypedArray() // it.length > 0 -> it.isNotEmpty() || isNotBlank()
+        // .let {if(it.isEmpty) arrayOf("EMPTY")} 한 줄
         return if(answer.isEmpty()) arrayOf("EMPTY") else answer
     }
 }
