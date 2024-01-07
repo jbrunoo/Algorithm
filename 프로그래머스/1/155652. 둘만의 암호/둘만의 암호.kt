5a91ffefb8aff4570c583985198014932bac1773
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-07 17:32:04 +0900

    [level 1] Title: 둘만의 암호, Time: 10.53 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun solution(s: String, skip: String, index: Int): String {
+        var stringBuilder = StringBuilder()
+        for(c in s) {
+            var tmp = c
+            for(i in 1..index) {
+                tmp++
+                if(tmp > 'z') tmp -= 26
+                while(tmp in skip) {
+                    tmp++
+                    if(tmp > 'z') tmp -= 26
+                }                    
+            }
+            // 1반례. 아스키코드 'z' == 122, 127 넘어간 뒤 -26
+            // 앞에서 -26 해주면 skip도 -26 해야함 
+            // for문 안에서 처리
+            // 2반례. while로 연속된 skip에 대응 
+            stringBuilder.append(tmp)
+        }
+        return stringBuilder.toString()
+    }
+}
