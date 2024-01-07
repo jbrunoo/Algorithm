8678f6148ff9a2697ff9d7e2d42e020e59d5164a
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-07 17:48:49 +0900

    [level 1] Title: 둘만의 암호, Time: 10.72 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652#) 
 
 ### 성능 요약
 
-메모리: 60.9 MB, 시간: 10.53 ms
+메모리: 61.2 MB, 시간: 10.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 0일 17:31:57
+2024년 1월 0일 17:48:45
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
@@ -1,22 +1,30 @@
 class Solution {
     fun solution(s: String, skip: String, index: Int): String {
-        var stringBuilder = StringBuilder()
-        for(c in s) {
-            var tmp = c
-            for(i in 1..index) {
-                tmp++
-                if(tmp > 'z') tmp -= 26
-                while(tmp in skip) {
-                    tmp++
-                    if(tmp > 'z') tmp -= 26
-                }                    
-            }
-            // 1반례. 아스키코드 'z' == 122, 127 넘어간 뒤 -26
-            // 앞에서 -26 해주면 skip도 -26 해야함 
-            // for문 안에서 처리
-            // 2반례. while로 연속된 skip에 대응 
-            stringBuilder.append(tmp)
+        // var stringBuilder = StringBuilder()
+        // for(c in s) {
+        //     var tmp = c
+        //     for(i in 1..index) {
+        //         tmp++
+        //         if(tmp > 'z') tmp -= 26
+        //         while(tmp in skip) {
+        //             tmp++
+        //             if(tmp > 'z') tmp -= 26
+        //         }                    
+        //     }
+        //     // 1반례. 아스키코드 'z' == 122, 127 넘어간 뒤 -26
+        //     // 앞에서 -26 해주면 skip도 -26 해야함 
+        //     // for문 안에서 처리
+        //     // 2반례. while로 연속된 skip에 대응 
+        //     stringBuilder.append(tmp)
+        // }
+        // return stringBuilder.toString()
+        
+        var answer: String = ""
+        val alphabet = ('a'..'z').filter { it !in skip }
+        s.forEach {
+            val i = (alphabet.indexOf(it) + index) % alphabet.size
+            answer += alphabet[i]
         }
-        return stringBuilder.toString()
+        return answer
     }
 }
