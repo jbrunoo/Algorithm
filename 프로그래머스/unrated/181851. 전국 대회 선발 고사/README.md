31ba3a11d7a0a4c31d9adba8d120d91887502b58
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-09 01:35:47 +0900

    [level unrated] Title: 전국 대회 선발 고사, Time: 16.43 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/README.md
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 전국 대회 선발 고사 - 181851 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181851) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181851#) 
 
 ### 성능 요약
 
-메모리: 64.2 MB, 시간: 16.30 ms
+메모리: 63.6 MB, 시간: 16.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 1:29:14
+2024년 1월 2일 1:35:45
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.kt
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.kt
@@ -1,12 +1,17 @@
 class Solution {
     fun solution(rank: IntArray, attendance: BooleanArray): Int {
-        var keyMap = mutableMapOf<Int, Int>()
-        rank.mapIndexed { idx, value ->
-            keyMap[value] = idx
-        }
-        val keys = keyMap.filter {
-            attendance[it.value]
-        }.keys.sorted()
-        return 10000 * keyMap[keys[0]]!! + 100 * keyMap[keys[1]]!! + keyMap[keys[2]]!!
+        // var keyMap = mutableMapOf<Int, Int>()
+        // rank.mapIndexed { idx, value ->
+        //     keyMap[value] = idx
+        // }
+        // val keys = keyMap.filter {
+        //     attendance[it.value]
+        // }.keys.sorted()
+        // return 10000 * keyMap[keys[0]]!! + 100 * keyMap[keys[1]]!! + keyMap[keys[2]]!!
+        
+        // pair 활용
+        val answer = rank.mapIndexed { i, v -> v to i }.filter { attendance[it.second] }.sortedBy { it.first }
+        return 10000 * answer[0].second + 100 * answer[1].second + answer[2].second
+        
     }
 }
