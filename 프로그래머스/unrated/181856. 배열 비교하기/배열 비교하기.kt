9bbce93d3b43c603173138e91f413e9c4ee1b470
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-08 12:37:59 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.02 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.kt
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.kt
@@ -1,7 +1,10 @@
 class Solution {
     fun solution(arr1: IntArray, arr2: IntArray): Int {
-        return if(arr1.size > arr2.size) 1
-        else if(arr1.size < arr2.size) -1
-        else arr1.sum().compareTo(arr2.sum())
+        // return if(arr1.size > arr2.size) 1
+        // else if(arr1.size < arr2.size) -1
+        // else arr1.sum().compareTo(arr2.sum())
+        val n = arr1.size.compareTo(arr2.size)
+        return if(n == 0) arr1.sum().compareTo(arr2.sum())
+        else n
     }
 }
