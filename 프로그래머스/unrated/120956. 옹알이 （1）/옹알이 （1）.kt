f641bc602d85885b959ae674973997ea61e09fd6
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-08 23:14:01 +0900

    [level unrated] Title: 옹알이 (1), Time: 2.00 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.kt
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.kt
@@ -1,12 +1,13 @@
 class Solution {
     fun solution(babbling: Array<String>): Int {
-        var sentence = arrayOf<String>("aya", "ye", "woo", "ma")
-        // 조합 14가지 경우의 수 list에 담아서 비교 이런건..?
-        // fold의 재발견
-        return babbling.map { b ->
-            sentence.fold(b) {acc, word ->
-                acc.replace(word, " ") // ""로 하면 반례 wyeoo x
-            }
-        }.filter { it.isBlank() }.count()
+        // var sentence = arrayOf<String>("aya", "ye", "woo", "ma")
+        // return babbling.map { b ->
+        //     sentence.fold(b) {acc, word ->
+        //         acc.replace(word, " ")
+        //     }
+        // }.filter { it.isBlank() }.count()
+        
+        var re = "aya|ye|woo|ma".toRegex()
+        return babbling.map { it.replace(re, "") }.count { it.isEmpty() } // blank는 empty보다 오래걸림
     }
 }
