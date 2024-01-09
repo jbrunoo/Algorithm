d1442efe2bd6c22bc79377e524e75c12cf2bbd27
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-10 01:56:39 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.14 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.kt
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.kt
@@ -1,11 +1,12 @@
 class Solution {
     fun solution(n: Int): Array<IntArray> {
-        var answer: Array<IntArray> = Array(n) { IntArray(n) }
-        for(j in 0 until n) {
-            for(i in 0 until n) {
-                if(i == j) answer[i][j] = 1
-            }
-        }
-        return answer
+        // var answer: Array<IntArray> = Array(n) { IntArray(n) }
+        // for(j in 0 until n) {
+        //     for(i in 0 until n) {
+        //         if(i == j) answer[i][j] = 1
+        //     }
+        // }
+        // return answer
+        return Array(n) {i -> IntArray(n) {j -> if(i == j) 1 else 0 } }
     }
 }
