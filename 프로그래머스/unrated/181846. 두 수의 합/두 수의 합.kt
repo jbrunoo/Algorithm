7154a4e3fc7408ac96553c0c8ffe39d7a143278c
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-10 00:03:27 +0900

    [level unrated] Title: 두 수의 합, Time: 285.71 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.kt
+++ 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.kt
@@ -1,6 +1,9 @@
 import java.math.BigInteger
 class Solution {
     fun solution(a: String, b: String): String {
+        // 배열로 나누어서 구현
+        // python3, java는 담을 수 있음.
+        // (to) BigInteger, BigDecimal
         val numA = BigInteger(a)
         val numB = BigInteger(b)
         val sum = numA.add(numB)
