33f310171492dfe9e68e9d3cd32fada8b98c9f2b
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-10 00:22:21 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.kt
@@ -0,0 +1,8 @@
+import kotlin.math.abs //java.lang.Math.abs(java)
+class Solution {
+    fun solution(a: Int, b: Int): Int {
+        return if(a % 2 == 1 && b % 2 == 1) a * a + b * b
+        else if(a % 2 == 0 && b % 2 == 0) abs(a - b)
+        else 2 * (a + b)
+    }
+}
