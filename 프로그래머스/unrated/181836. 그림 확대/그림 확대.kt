9f26d418a43945324fdc97e1a1ba6ff720722fe2
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-10 02:54:13 +0900

    [level unrated] Title: 그림 확대, Time: 20.79 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.kt
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.kt
@@ -2,9 +2,7 @@ class Solution {
     fun solution(picture: Array<String>, k: Int): Array<String> {
         var answer: Array<String> = arrayOf<String>()
         for(str in picture) {
-            for(i in 1..k) {
-                answer += str.map { it.toString().repeat(k) }.joinToString("")
-            }
+              repeat(k) { answer += str.map { it.toString().repeat(k) }.joinToString("") }
         }
         return answer
     }
