70eb86ed0ec4b5e8802bc7d81f87da43987ff994
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-10 01:09:15 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 19.78 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.kt
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.kt
@@ -1,6 +1,7 @@
 class Solution {
     fun solution(arr: IntArray, k: Int): IntArray {
-        return if(k % 2 == 1) arr.map { it * k}.toIntArray()
-        else arr.map { it + k }.toIntArray()
+        // return if(k % 2 == 1) arr.map { it * k}.toIntArray()
+        // else arr.map { it + k }.toIntArray()
+        return arr.map { if(k % 2 == 1) it * k else it + k }.toIntArray()
     }
 }
