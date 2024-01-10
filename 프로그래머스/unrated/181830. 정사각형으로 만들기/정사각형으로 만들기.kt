37c19fa73d11556793a70513fe1e1eea3301f8cc
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-01-11 02:01:33 +0900

    [level unrated] Title: 정사각형으로 만들기, Time: 0.07 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.kt
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.kt
@@ -1,15 +1,26 @@
+import kotlin.math.max
 class Solution {
     fun solution(arr: Array<IntArray>): Array<IntArray> {
-        var answer: Array<IntArray> = arr
-        val diff = arr.size - arr[0].size 
-        if(diff > 0) {
-            for(i in arr.indices) {
-                answer[i] += IntArray(diff)
-            }
-        } 
-        else if(diff < 0) {
-            answer += Array(-diff) { IntArray(arr[0].size) }
+        // var answer: Array<IntArray> = arr
+        // val diff = arr.size - arr[0].size 
+        // if(diff > 0) {
+        //     for(i in arr.indices) {
+        //         answer[i] += IntArray(diff)
+        //     }
+        // } 
+        // else if(diff < 0) {
+        //     answer += Array(-diff) { IntArray(arr[0].size) }
+        // }
+        // return answer
+        
+        // size를 계산해서 미리 array를 만들고 arr을 붙임.
+        // 매우 빠름..
+        val max = max(arr.size, arr[0].size)
+        val array = Array(max) { IntArray(max) }
+
+        for (i in arr.indices) {
+            System.arraycopy(arr[i], 0, array[i], 0, arr[0].size)
         }
-        return answer
+        return array
     }
 }
