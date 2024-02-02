cdff5f0adc76348087291b7920435a87f227d8ef
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-02-02 12:28:43 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 8.36 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.kt
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun solution(my_str: String, n: Int): Array<String> {
-        var answer: Array<String> = arrayOf<String>()
-        answer += my_str.chunked(n)
-        return answer
+        return my_str.chunked(n).toTypedArray()
     }
 }
