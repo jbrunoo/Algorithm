5bfec1c0effc34e043feb1ac3e4f66a1388e29ec
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-06-10 12:19:01 +0900

    [level 2] Title: 문자열 압축, Time: 28.95 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
 
 ### 성능 요약
 
-메모리: 61.5 MB, 시간: 55.10 ms
+메모리: 61.1 MB, 시간: 28.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 10일 11:50:11
+2024년 06월 10일 12:18:59
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.kt
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.kt
@@ -1,29 +1,29 @@
 class Solution {
     fun solution(s: String): Int {
-        var answerList = mutableListOf<String>()
-        
+        var wordList = mutableListOf<String>()
+
         for(i in 1..s.length / 2 + 1) {
-            var answer = ""
-            val splitStr = s.chunked(i)
+            var word = ""
+            val subList = s.chunked(i)
             var sum = 0
             var idx = 0
-            splitStr.forEachIndexed { j, v ->
-                if(splitStr[idx] == v) {
+            subList.forEachIndexed { j, v ->
+                if(subList[idx] == v) {
                     sum++
                 } else {
-                    if(sum !in 0..1) answer += sum.toString() + splitStr[j-1]
-                    else answer += splitStr[j-1]
+                    if(sum !in 0..1) word += sum.toString() + subList[j-1]
+                    else word += subList[j-1]
                     idx = j
                     sum = 1
                 }
-                if(j == splitStr.lastIndex) {
-                    if(sum !in 0..1) answer += sum.toString() + v
-                    else answer += v
+                if(j == subList.lastIndex) {
+                    if(sum !in 0..1) word += sum.toString() + v
+                    else word += v
                 }
             }
-            answerList.add(answer)
+            wordList.add(word)
         }
-        
-        return answerList.map { it.length }.minOrNull()!!
+
+        return wordList.map { it.length }.minOrNull()!!
     }
 }
