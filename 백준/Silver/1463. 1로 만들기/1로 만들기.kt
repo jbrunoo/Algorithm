3243cfa73e722677a224f05ebadbbe07a511151e
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-07-21 23:59:06 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 16620 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.kt
@@ -0,0 +1,13 @@
+import kotlin.math.min
+
+fun main() = with(System.`in`.bufferedReader()){
+    val n = readLine().toInt()
+    val dp = IntArray(n + 1) { 0 }
+
+    for(i in 2..n) {
+        dp[i] = dp[i - 1] + 1
+        if(i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1)
+        if(i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1)
+    }
+    println(dp[n])
+}
