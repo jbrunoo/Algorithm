f96d03f7e1a4706239d37e3072e29d75c19833cf
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-07-30 18:43:16 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 792 ms, Memory: 79244 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.kt
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.kt
@@ -3,22 +3,15 @@ import java.util.*
 fun main() = with(System.`in`.bufferedReader()) {
     val sb = StringBuilder()
     val (m, n) = readLine().split(" ").map { it.toInt() }
-    val arr = IntArray(m)
+    val dp = IntArray(m + 1)
     val st = StringTokenizer(readLine())
-    for(i in 0 until m) {
-        arr[i] += st.nextToken().toInt()
+    for(i in 1..m) {
+        dp[i] = dp[i - 1] + st.nextToken().toInt()
     }
-    val memo = IntArray(m + 1)
 
     repeat(n) {
         val (a, b) = readLine().split(" ").map { it.toInt() }
-        for(i in arr.indices) {
-            if(memo[i + 1] == 0) memo[i + 1] = memo[i] + arr[i]
-            if(memo[b] != 0) {
-                sb.appendLine(memo[b] - memo[a - 1])
-                break
-            }
-        }
+        sb.appendLine(dp[b] - dp[a - 1])
     }
     println(sb)
 }
