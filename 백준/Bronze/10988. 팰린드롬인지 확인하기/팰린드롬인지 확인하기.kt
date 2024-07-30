d13079efe700146822aa7994fe646d2e30c244a8
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-07-30 19:45:58 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 80 ms, Memory: 12216 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.kt
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.kt
@@ -1,5 +1,11 @@
 fun main() = with(System.`in`.bufferedReader()) {
     val s = readLine()
-    if(s == s.reversed()) println(1)
-    else println(0)
+    val e = s.length
+    for(i in 0..s.length / 2) {
+        if(s[i] != s[e - i - 1]) {
+            println(0)
+            return
+        }
+    }
+    println(1)
 }
