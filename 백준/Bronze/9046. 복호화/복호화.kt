e8d2ba369c7f441f60d3a519569a22e6b3a81e6d
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-08-02 20:13:31 +0900

    [Bronze II] Title: 복호화, Time: 136 ms, Memory: 18740 KB -BaekjoonHub

--- 백준/Bronze/9046. 복호화/복호화.kt
+++ 백준/Bronze/9046. 복호화/복호화.kt
@@ -2,15 +2,16 @@ fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
 
     repeat(n) {
-        val s = readLine().replace(" ","")
+        val s = readLine()
         val alphabet = IntArray(26)
 
         for(c in s) {
-            alphabet[c.code - 97]++
+            if(c != ' ') alphabet[c - 'a']++
         }
-        val maxValue = alphabet.max()
-        val maxAlphabet = alphabet.filter { it == maxValue }
-        if (maxAlphabet.count() == 1) println((alphabet.indexOf(maxValue) + 97).toChar())
-        else println("?")
+
+        val max = alphabet.max()
+        val cnt = alphabet.count { it == max }
+        if(cnt != 1) println("?")
+        else println((alphabet.indexOf(max) + 97).toChar())
     }
 }
