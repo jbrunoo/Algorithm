b4c9f8a6e2387fe69ba2533768503530ba63dfd6
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-08-09 22:44:07 +0900

    [Silver III] Title: 파도반 수열, Time: 96 ms, Memory: 12336 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.kt
@@ -0,0 +1,28 @@
+val dp = Array<Long>(101) { 0 }
+var start = 4
+
+fun main() = with(System.`in`.bufferedReader()) {
+    dp[0] = -1
+    dp[1] = 1
+    dp[2] = 1
+    dp[3] = 1
+
+    val n1 = readLine().toInt()
+
+    repeat(n1) {
+        val n2 = readLine().toInt()
+        println(pado(n2))
+    }
+
+}
+
+fun pado(n: Int): Long {
+    if(n < 4 || dp[n] != 0L) return dp[n]
+
+    for(i in start..n) {
+        dp[i] = dp[i - 2] + dp[i - 3]
+    }
+    start = n
+
+    return dp[n]
+}
