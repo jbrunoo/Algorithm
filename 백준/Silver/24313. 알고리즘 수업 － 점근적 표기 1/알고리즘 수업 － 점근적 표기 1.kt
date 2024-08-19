9bce5c8ecd4856225e2d0cf62e1a6aa658d5e177
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-08-19 23:45:32 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 76 ms, Memory: 12248 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.kt
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.kt
@@ -4,14 +4,6 @@ fun main() = with(System.`in`.bufferedReader()){
     val a1 = st.nextToken().toInt()
     val a0 = st.nextToken().toInt()
     val c = readLine().toInt()
-    var n = readLine().toInt()
-
-    while(n <= 100) {
-        if(a1 * n + a0 > c * n) {
-            print(0)
-            break
-        }
-        n++
-    }
-    if(n == 101) print(1)
+    val n = readLine().toInt()
+    print(if(a1 * n + a0 <= c * n && a1 <= c) "1" else "0")
 }
