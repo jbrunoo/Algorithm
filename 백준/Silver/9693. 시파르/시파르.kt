9f4878926a0373a4b97990076d6cbd2a93906e99
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-08-21 17:11:18 +0900

    [Silver IV] Title: 시파르, Time: 76 ms, Memory: 12128 KB -BaekjoonHub

--- 백준/Silver/9693. 시파르/시파르.kt
+++ 백준/Silver/9693. 시파르/시파르.kt
@@ -1,18 +1,17 @@
 fun main() = with(System.`in`.bufferedReader()) {
     var idx = 0
-    while(true) {
+
+    while (true) {
         idx++
-        val n = readLine().toInt()
-        if(n == 0) break
+        var n = readLine().toInt()
+        if (n == 0) break
 
         var cnt = 0
-        for(i in 5..n step 5) {
-            var tmp = i
-            while(tmp % 5 == 0) {
-                cnt++
-                tmp /= 5
-            }
+        while (n >= 5) {
+            cnt += n / 5
+            n /= 5
         }
+
         println("Case #$idx: $cnt")
     }
 }
