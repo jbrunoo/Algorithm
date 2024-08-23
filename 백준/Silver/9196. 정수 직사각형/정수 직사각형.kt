5ef1e3ad40caf82638a739a58b45154db1a7d852
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-08-24 00:51:51 +0900

    [Silver V] Title: 정수 직사각형, Time: 268 ms, Memory: 24104 KB -BaekjoonHub

--- 백준/Silver/9196. 정수 직사각형/정수 직사각형.kt
+++ 백준/Silver/9196. 정수 직사각형/정수 직사각형.kt
@@ -1,30 +1,31 @@
 import java.util.*
-import kotlin.math.sqrt
 
 fun main() = with(System.`in`.bufferedReader()) {
     val sb = StringBuilder()
-    val arr = Array(151) { Array(151) { Square(0, 0, 0.0) } }
+    val li = mutableListOf<Pair<Int, Int>>()
 
-    for (i in 1..149) {
-        for (j in i + 1..150) {
-            arr[i][j] = Square(i, j, sqrt(i.toDouble() * i + j * j))
+    for(i in 1..149) {
+        for(j in i + 1..150) {
+            li.add(Pair(i, j))
         }
     }
-    val flatArr = arr.flatMap { it.toList() }.sortedWith(
-        compareBy({ it.area }, { it.h })
+
+    li.sortWith(
+        compareBy(
+            { it.first * it.first + it.second * it.second },
+            { it.first }
+        )
     )
 
-    while (true) {
+    while(true) {
         val st = StringTokenizer(readLine())
         val h = st.nextToken().toInt()
         val w = st.nextToken().toInt()
-        if (h <= 0 || w <= 0) break
+        if(h == 0) break
 
-
-        val ans = flatArr.indexOf(arr[h][w])
-        sb.appendLine("${flatArr[ans + 1].h} ${flatArr[ans + 1].w}")
+        val ans = li.indexOf(Pair(h, w))
+        sb.appendLine("${li[ans + 1].first} ${li[ans + 1].second}")
     }
+
     print(sb)
 }
-
-data class Square(val h:Int = 0, val w:Int = 0, val area:Double = 0.0)
