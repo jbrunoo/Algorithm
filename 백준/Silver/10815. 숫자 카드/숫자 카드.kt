141a753d42c53440055c7705a5dbaa8db9f34e2c
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-08-26 01:33:26 +0900

    [Silver V] Title: 숫자 카드, Time: 828 ms, Memory: 191372 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.kt
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.kt
@@ -3,18 +3,24 @@ import java.util.*
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
     val st1 = StringTokenizer(readLine())
-    val map = HashMap<Int, Int>()
-    for(i in 1..n) { map[st1.nextToken().toInt()] = 1 }
+    val map = hashMapOf<Int, Int>()
+    while(st1.hasMoreTokens()) {
+        map[st1.nextToken().toInt()] = 1
+    }
     val m = readLine().toInt()
     val st2 = StringTokenizer(readLine())
     val arr2 = IntArray(m) { st2.nextToken().toInt() }
 
     val sb = StringBuilder()
 
-    for(i in arr2) {
-        if(map[i] == 1) sb.append("1") else sb.append("0")
-        if(i != arr2.last()) sb.append(" ")
+
+    val last = arr2[m-1]
+
+    for (i in 0 until m - 1) {
+        val ans = if(map.containsKey(arr2[i])) 1 else 0
+        sb.append("$ans ")
     }
+    sb.append("${if(map.containsKey(arr2[m-1])) 1 else 0}")
 
     print(sb)
 }
