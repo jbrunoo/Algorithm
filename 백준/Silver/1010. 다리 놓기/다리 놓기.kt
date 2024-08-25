e1b5f8d38087c5eb0ec10d8f8416e60ab0dc6aeb
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-08-25 20:09:45 +0900

    [Silver V] Title: 다리 놓기, Time: 76 ms, Memory: 12632 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.kt
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.kt
@@ -1,24 +1,29 @@
 import java.util.*
 
+val arr = Array(31) { IntArray(31) }
+
 fun main() = with(System.`in`.bufferedReader()) {
     val t = readLine().toInt()
-    val arr = Array(31) { IntArray(31) }
+    val sb = StringBuilder()
 
-    for(i in 1..30) {
-        for(j in i..30) {
-            when (i) {
-                j -> arr[i][j] = 1
-                1 -> arr[i][j] = j
-                else -> arr[i][j] += arr[i-1][j-1] + arr[i][j-1]
-            }
-        }
+    for (i in 1..30) {
+        arr[1][i] = i
+        arr[i][i] = 1
     }
-    
+
     repeat(t) {
         val st = StringTokenizer(readLine())
         val n = st.nextToken().toInt()
         val m = st.nextToken().toInt()
 
-        println(arr[n][m])
+        sb.appendLine(dp(n, m))
     }
+
+    print(sb)
+}
+
+fun dp(n: Int, m: Int): Int {
+    if (arr[n][m] != 0) return arr[n][m]
+    arr[n][m] = dp(n - 1,m - 1) + dp(n, m - 1)
+    return arr[n][m]
 }
