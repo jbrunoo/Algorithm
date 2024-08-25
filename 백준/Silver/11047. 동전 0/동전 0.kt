30e0d9d17953ef5273302b72bb4a5074a0dfd283
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-08-25 21:15:59 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 21176 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.kt
+++ 백준/Silver/11047. 동전 0/동전 0.kt
@@ -2,14 +2,14 @@ fun main() = with(System.`in`.bufferedReader()) {
     val (n, k) = readLine().split(" ").map{ it.toInt() }
     val arr = IntArray(n)
 
-    repeat(n) {
-        arr[it] = readLine().toInt()
+    for(i in n - 1 downTo 0) {
+        arr[i] = readLine().toInt()
     }
 
     var ans = 0
     var change = k
 
-    for(i in n - 1 downTo 0) {
+    for(i in 0 until n) {
         if(change >= arr[i]) {
             ans += change / arr[i]
             change %= arr[i]
