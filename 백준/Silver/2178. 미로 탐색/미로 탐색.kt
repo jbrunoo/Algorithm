257ad79776d9dd8b9d019a757be12fb7c6e78f64
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-08-27 15:32:56 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 21900 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.kt
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.kt
@@ -1,5 +1,9 @@
 import java.util.*
 
+val dx = intArrayOf(0, 1, 0, -1)
+val dy = intArrayOf(-1, 0, 1, 0)
+data class Node(val x: Int, val y: Int)
+
 fun main() = with(System.`in`.bufferedReader()) {
     val (n, m) = readLine().split(" ").map { it.toInt() }
 
@@ -9,7 +13,7 @@ fun main() = with(System.`in`.bufferedReader()) {
     repeat(n) {
         val str = readLine()
         for(i in 0 until m) {
-            arr[it][i] = str[i].digitToInt()
+            arr[it][i] = str[i] - '0'
         }
     }
 
@@ -18,22 +22,19 @@ fun main() = with(System.`in`.bufferedReader()) {
 }
 
 fun bfs(a: Int, b: Int, arr: Array<IntArray>, visited: Array<IntArray>) {
-    val queue = LinkedList<Pair<Int, Int>>()
+    val queue = LinkedList<Node>()
     visited[a][b] = 1
-    queue.add(Pair(a, b))
-
-    val dx = intArrayOf(0, 1, 0, -1)
-    val dy = intArrayOf(-1, 0, 1, 0)
+    queue.add(Node(a,b))
 
     while(queue.isNotEmpty()) {
-        val (x, y) = queue.pop()
+        val (x, y) = queue.poll()
         for (i in 0..3) {
             val nx = x + dx[i]
             val ny = y + dy[i]
 
             if(nx < 0 || ny < 0 || nx >= arr.size || ny >= arr[0].size) continue
             if(arr[nx][ny] == 1 && visited[nx][ny] == 0) {
-                queue.add(Pair(nx, ny))
+                queue.offer(Node(nx, ny))
                 visited[nx][ny] = visited[x][y] + 1
             }
         }
