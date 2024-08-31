0a85a368d9b39d5b9af5a090336d80bea1dc3ec7
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-08-31 18:59:34 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 88 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.kt
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.kt
@@ -1,63 +1,36 @@
-val vowel = "aeiou"
+const val VOWEL = "aeiou"
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val format1 = "> is acceptable."
-    val format2 = "> is not acceptable."
     val sb = StringBuilder()
 
-    while(true) {
+    while (true) {
         val word = readLine()
-        if(word == "end") break
+        if (word == "end") break
 
-        var isAcceptable = rule1(word)
-        if(!isAcceptable) {
-            sb.append("<$word").appendLine(format2)
-            continue
-        }
-        isAcceptable = rule2(word)
-        if(!isAcceptable) {
-            sb.append("<$word").appendLine(format2)
-            continue
-        }
-        isAcceptable = rule3(word)
-        if(!isAcceptable) {
-            sb.append("<$word").appendLine(format2)
-            continue
-        }
-        sb.append("<$word").appendLine(format1)
+        val isAcceptable = rule(word)
+        sb.appendLine("<$word> is ${if(isAcceptable)"acceptable" else "not acceptable"}.")
     }
 
     print(sb)
 }
 
-fun rule1(word: String): Boolean {
-    for(v in vowel) if(v in word) return true
-    return false
-}
+fun rule(word: String): Boolean {
+    var hasVowel = false
+    var vo = 0
+    var co = 0
+    var pre = ' '
 
-
-fun rule2(word: String): Boolean {
-    val len = word.length
-    if(len < 3) return true
-    for(i in 0..len-3) {
-        var vo = 0
-        var co = 0
-        for(c in word.substring(i, i+3)) {
-            if(c in vowel) vo++
-            if(c !in vowel) co++
+    for (c in word) {
+        if (c in VOWEL) {
+            hasVowel = true
+            vo++; co = 0
+        } else {
+            co++; vo = 0
         }
-        if(vo == 3 || co == 3) return false
+        if (vo == 3 || co == 3) return false
+        if (c != 'e' && c != 'o' && pre == c) return false
+        pre = c
     }
-    return true
-}
 
-
-fun rule3(word: String): Boolean {
-    val len = word.length
-    if(len < 2) return true
-    for(i in 0..len-2) {
-        if(word[i] == 'e' || word[i] == 'o') continue
-        if(word[i] == word[i+1]) return false
-    }
-    return true
+    return hasVowel
 }
