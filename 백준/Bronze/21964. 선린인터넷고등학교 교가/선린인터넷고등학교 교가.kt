e1c515917ca5ca13939aa565fe129f31f9bf0075
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-09-01 23:45:40 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 80 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.kt
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.kt
@@ -1,1 +1,5 @@
-fun main(){readln();print(readln().takeLast(5))}
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val s = readLine()
+    print(s.substring(n-5))
+}
