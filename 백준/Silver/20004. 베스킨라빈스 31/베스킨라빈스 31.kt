e28c8d489df924faba91cb43fbd6f1594efdde25
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-09-02 18:54:50 +0900

    [Silver IV] Title: 베스킨라빈스 31, Time: 76 ms, Memory: 12200 KB -BaekjoonHub

--- 백준/Silver/20004. 베스킨라빈스 31/베스킨라빈스 31.kt
+++ 백준/Silver/20004. 베스킨라빈스 31/베스킨라빈스 31.kt
@@ -1,12 +1,5 @@
 fun main() = with(System.`in`.bufferedReader()) {
-    val sb = StringBuilder()
-    val a = readLine().toInt()
-
-    for(n in 1..a) {
-        val min = 30 % (n + 1)
-        val win = if(min == 0) n + 1 else min
-        if(win !in 1..n) sb.appendLine(n)
-    }
-
+    val sb = StringBuilder(); val a = readLine().toInt()
+    for(n in 1..a) if(30 % (n + 1) == 0) sb.appendLine(n)
     print(sb)
 }
