2d1435701d54502e7913b1bfa150700d1508c197
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-09-03 15:30:00 +0900

    [Silver IV] Title: 괄호, Time: 76 ms, Memory: 12384 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.kt
+++ 백준/Silver/9012. 괄호/괄호.kt
@@ -1,28 +1,18 @@
-var cnt = 0
-
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
     val sb = StringBuilder()
 
     repeat(n) {
         val s = readLine()
-        sb.appendLine(if(vps(s)) "YES" else "NO" )
+        var cnt = 0
+
+        for(c in s) {
+            if(cnt < 0) break
+            if(c == '(') cnt++
+            else cnt--
+        }
+        sb.appendLine(if(cnt==0) "YES" else "NO")
     }
 
     print(sb)
-}
-
-fun vps(s: String): Boolean {
-    var cnt = 0
-
-    for(c in s) {
-        if(c == '(') {
-            cnt++
-        } else {
-            if(cnt == 0) return false
-            else cnt--
-        }
-    }
-
-    return cnt == 0
 }
