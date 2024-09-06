37c6d831f850f874cc57a9596da3ee69856be212
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-09-07 02:49:21 +0900

    [Bronze II] Title: 상수, Time: 72 ms, Memory: 12252 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.kt
+++ 백준/Bronze/2908. 상수/상수.kt
@@ -2,7 +2,19 @@ import java.util.*
 
 fun main() = with(System.`in`.bufferedReader()) {
     val st = StringTokenizer(readLine())
-    fun i(): Int = st.nextToken().reversed().toInt()
+    fun i(): String = st.nextToken()
     val a = i(); val b = i()
-    print(if(a > b) a else b)
+    val a1 = a[2] + a[1].toString() + a[0]
+    val b1 = b[2] + b[1].toString() + b[0]
+
+    if(a[2] > b[2]) print(a1)
+    else if(a[2] < b[2]) print(b1)
+    else {
+        if(a[1] > b[1]) print(a1)
+        else if(a[1] < b[1]) print(b1)
+        else {
+            if(a[0] > b[0]) print(a1)
+            else print(b1)
+        }
+    }
 }
