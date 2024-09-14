cdbbb52d3de0203e6cd89275267ec029c4231f27
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-09-14 18:28:33 +0900

    [Silver IV] Title: 파스칼 삼각형, Time: 76 ms, Memory: 12272 KB -BaekjoonHub

--- 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.kt
+++ 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.kt
@@ -3,9 +3,9 @@ private lateinit var dp:Array<IntArray>
 fun main()=StreamTokenizer(System.`in`.bufferedReader()).run{
     fun i():Int{nextToken();return nval.toInt()}
     val r=i()-1;val c=i()-1;val w=i()
-    dp=Array(29){IntArray(29)}
-    var cnt=0;for(i in 0..w-1)for(j in 0..i)cnt+=p(r+i,c+j)
-    print(cnt) 
+    dp=Array(r+w){ IntArray(r+w)}
+    var cnt=0;for(i in 0.. w-1)for(j in 0..i)cnt+=p(r+i,c+j)
+    print(cnt)
 }
 fun p(r:Int,c:Int):Int{
     if(c==0||c==r)return 1
