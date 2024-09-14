3760f219db2484907baf99f1239a3feab1bd051b
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-09-14 15:10:11 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 708 ms, Memory: 66868 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.kt
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.kt
@@ -1,18 +1,12 @@
 import java.util.*
-
 fun main() = with(System.`in`.bufferedReader()) {
-    val n = readLine().toInt()
-    val map = HashMap<String, Int>()
-
+    val n = readLine().toInt(); val map = HashMap<String, Int>()
     repeat(n) {
-        val st = StringTokenizer(readLine())
-        val name = st.nextToken(); val memo = st.nextToken()
-
-        if(memo == "enter") map[name] = 1
-        else map[name] = 0
+        val st = StringTokenizer(readLine()); val name = st.nextToken(); val memo = st.nextToken()
+        if(memo == "enter") map[name] = 1 else map[name] = 0
     }
-
-    map.filter { it.value == 1 }.keys.sortedDescending().forEach { 
-        println(it)
-    }
+    val m = map.filter { it.value == 1 }.keys.sortedDescending()
+    val sb = StringBuilder()
+    for(name in m) sb.appendLine(name)
+    print(sb)
 }
