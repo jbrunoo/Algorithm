c21a5c3ba9e35247473225c34f5242da11cb90ca
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-09-19 16:24:20 +0900

    [Bronze I] Title: 배고픈 아리의 샌드위치 구매하기, Time: 76 ms, Memory: 12184 KB -BaekjoonHub

--- 백준/Bronze/25166. 배고픈 아리의 샌드위치 구매하기/배고픈 아리의 샌드위치 구매하기.kt
+++ 백준/Bronze/25166. 배고픈 아리의 샌드위치 구매하기/배고픈 아리의 샌드위치 구매하기.kt
@@ -1,6 +1,5 @@
-fun main() = with(System.`in`.bufferedReader()) {
-    val(s,m)=readLine().split(" ").map{it.toInt()}
-    if(s<1024)print("No thanks")
-    else if(m and s-1023==s-1023)print("Thanks")
-    else print("Impossible")
+import java.util.*
+fun main()=with(System.`in`.bufferedReader()){
+    val st=StringTokenizer(readLine());val s=st.nextToken().toInt();val m=st.nextToken().toInt()
+    if(s<1024)print("No thanks")else if(m and s-1023==s-1023)print("Thanks")else print("Impossible")
 }
