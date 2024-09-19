902e21a7b0da1bedbb54b1ddb4d1d0b5747dbf94
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-09-19 17:08:55 +0900

    [Silver V] Title: 암호해독기, Time: 292 ms, Memory: 27720 KB -BaekjoonHub

--- 백준/Silver/17176. 암호해독기/암호해독기.kt
+++ 백준/Silver/17176. 암호해독기/암호해독기.kt
@@ -1,15 +1,32 @@
 import java.util.*
-fun main()=with(System.`in`.bufferedReader()){
+
+fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
     val st = StringTokenizer(readLine())
-    val arr = Array(n) {
-        when(val c = st.nextToken().toInt()) {
-            0 -> {' '}
-            in 1..26 -> {'A'+c-1}
-            else -> {'a'+c-27}
+    val map = HashMap<Char, Int>()
+    repeat(n) {
+        when (val c = st.nextToken().toInt()) {
+            0 -> {
+                if(map.containsKey(' ')) map[' '] = map[' ']!! + 1
+                else map[' '] = 1
+            }
+            in 1..26 -> {
+                if(map.containsKey('A' + c - 1)) map['A' + c - 1] = map['A' + c - 1]!! + 1
+                else map['A' + c - 1] = 1
+            }
+            else -> {
+                if(map.containsKey('a' + c - 27)) map['a' + c - 27] = map['a' + c - 27]!! + 1
+                else map['a' + c - 27] = 1
+            }
         }
     }
     val str = readLine()
-    arr.sort()
-    if(arr.joinToString()==str.toList().sorted().joinToString()) print("y") else print("n")
+    for(c in str) {
+        if(map.containsKey(c) && map[c]!! > 0) map[c] = map[c]!! - 1
+        else {
+            print("n")
+            return
+        }
+    }
+    print("y")
 }
