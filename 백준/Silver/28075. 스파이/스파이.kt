6374896c61fb250e9d1c4af3a6fba699a245d173
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-10-04 16:55:15 +0900

    [Silver III] Title: 스파이, Time: 92 ms, Memory: 12476 KB -BaekjoonHub

--- 백준/Silver/28075. 스파이/스파이.kt
+++ 백준/Silver/28075. 스파이/스파이.kt
@@ -1,6 +1,5 @@
 import kotlin.math.abs
 private lateinit var arr: IntArray
-private lateinit var ret: IntArray
 var n = 0
 var m = 0
 var cnt = 0
@@ -9,19 +8,17 @@ fun main()=java.io.StreamTokenizer(System.`in`.bufferedReader()).run{
     fun i():Int{nextToken();return nval.toInt()}
     n=i(); m=i()
     arr = IntArray(6){i()}
-    ret = IntArray(n)
     calc()
     print(cnt)
 }
 
-fun calc(prevIdx : Int = -10, depth: Int = 0) {
+fun calc(prevIdx : Int = -10, depth: Int = 0, sum: Int = 0) {
     if(depth == n) {
-        if(m <= ret.sum()) cnt++
+        if(sum >= m) cnt++
         return
     }
     for((i, v) in arr.withIndex()) {
-        ret[depth] = if(i == prevIdx || abs(prevIdx - i) == 3) v / 2 else v
-        calc(i, depth+1)
-        ret[depth] = 0
+        val nextValue = if(i == prevIdx || abs(prevIdx - i) == 3) v / 2 else v
+        calc(i, depth+1, sum + nextValue)
     }
 }
