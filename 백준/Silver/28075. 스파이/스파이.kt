4787d855ac467488ed9ca1de5442ace9f0e7243b
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-10-04 16:51:41 +0900

    [Silver III] Title: 스파이, Time: 140 ms, Memory: 18640 KB -BaekjoonHub

--- 백준/Silver/28075. 스파이/스파이.kt
+++ 백준/Silver/28075. 스파이/스파이.kt
@@ -1,6 +1,5 @@
 import kotlin.math.abs
 private lateinit var arr: IntArray
-private lateinit var visited: BooleanArray
 private lateinit var ret: IntArray
 var n = 0
 var m = 0
@@ -10,7 +9,6 @@ fun main()=java.io.StreamTokenizer(System.`in`.bufferedReader()).run{
     fun i():Int{nextToken();return nval.toInt()}
     n=i(); m=i()
     arr = IntArray(6){i()}
-    visited = BooleanArray(6)
     ret = IntArray(n)
     calc()
     print(cnt)
@@ -23,9 +21,7 @@ fun calc(prevIdx : Int = -10, depth: Int = 0) {
     }
     for((i, v) in arr.withIndex()) {
         ret[depth] = if(i == prevIdx || abs(prevIdx - i) == 3) v / 2 else v
-        visited[i] = true
         calc(i, depth+1)
         ret[depth] = 0
-        visited[i] = false
     }
 }
