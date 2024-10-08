4b4b737b5dce2e9ec7238a50a281ba97b8178d6a
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-10-08 19:12:15 +0900

    [Gold V] Title: 보물섬, Time: 476 ms, Memory: 193780 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.kt
+++ 백준/Gold/2589. 보물섬/보물섬.kt
@@ -1,6 +1,6 @@
 import java.util.*
 
-private lateinit var arr: Array<IntArray>
+private lateinit var arr: Array<CharArray>
 private lateinit var visited: Array<IntArray>
 private var n: Int = 0
 private var m: Int = 0
@@ -8,29 +8,22 @@ private var m: Int = 0
 fun main() = with(System.`in`.bufferedReader()) {
     val st = StringTokenizer(readLine())
     n = st.nextToken().toInt(); m = st.nextToken().toInt()
-    arr = Array(n) { IntArray(m) }
+    arr = Array(n) { CharArray(m) }
 
     repeat(n) {
         val row = readLine()
         for ((i, c) in row.withIndex()) {
-            if (c == 'L') arr[it][i] = 1
-            else arr[it][i] = 0
+            arr[it][i] = c
         }
     }
 
-    visited = Array(n) { IntArray(m) }
     var minDist = 0
 
-    for(i in 0 until n) {
-        for(j in 0 until m) {
-            if(arr[i][j] == 1) {
-                for (a in 0 until n) {
-                    for (b in 0 until m) {
-                        visited[a][b] = 0
-                    }
-                }
-                val dist = bfs(i, j)
-                minDist = kotlin.math.max(minDist, dist)
+    for (i in 0 until n) {
+        for (j in 0 until m) {
+            if (arr[i][j] == 'L') {
+                visited = Array(n) { IntArray(m) }
+                minDist = kotlin.math.max(minDist, bfs(i, j))
             }
         }
     }
@@ -42,10 +35,10 @@ val dx = listOf(1, -1, 0, 0)
 val dy = listOf(0, 0, 1, -1)
 
 fun bfs(a: Int, b: Int): Int {
-    val queue = ArrayDeque<Pair<Int, Int>>()
-    var dist = 0
-    queue.offer(Pair(a, b))
+    val queue = ArrayDeque<Pos>()
+    queue.offer(Pos(a, b))
     visited[a][b] = 1
+    var dist = 0
 
     while (queue.isNotEmpty()) {
         val (x, y) = queue.poll()
@@ -54,15 +47,14 @@ fun bfs(a: Int, b: Int): Int {
             val nx = x + dx[i]
             val ny = y + dy[i]
 
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue
-            if(arr[nx][ny] == 1 && visited[nx][ny] == 0) {
-                visited[nx][ny] = visited[x][y] + 1
-                dist = visited[nx][ny]
-                queue.offer(Pair(nx, ny))
-            }
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m || arr[nx][ny] != 'L' || visited[nx][ny] != 0) continue
+            visited[nx][ny] = visited[x][y] + 1
+            dist = visited[nx][ny]
+            queue.offer(Pos(nx, ny))
         }
     }
 
     return dist - 1
 }
 
+data class Pos(val x: Int, val y: Int)
