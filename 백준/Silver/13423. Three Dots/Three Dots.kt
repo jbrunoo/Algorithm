b80a6163491bfba6abed27c565ddd0c415a3207a
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-10-10 16:08:33 +0900

    [Silver II] Title: Three Dots, Time: 1104 ms, Memory: 22244 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.kt
+++ 백준/Silver/13423. Three Dots/Three Dots.kt
@@ -1,34 +1,17 @@
-lateinit var arr: IntArray
-lateinit var select: ArrayList<Int>
-var same = 0
-
 fun main() = java.io.StreamTokenizer(System.`in`.bufferedReader()).run {
     fun i():Int{nextToken();return nval.toInt()};val sb=StringBuilder()
-    select = ArrayList()
     repeat(i()) {
-        arr = IntArray(i()) { i() }
-        arr.sort()
-        combi()
+        var same = 0
+        val li = IntArray(i()) { i() }.sorted().toIntArray()
+
+        for(a in 0 until li.size) {
+            for(b in a + 1 until li.size) {
+                val dist = li[b] - li[a]
+                if(dist % 2 == 0 && li.binarySearch(li[a] + dist / 2) >= 0) same++
+            }
+        }
+
         sb.appendLine(same)
-        same = 0
-        select.clear()
     }
     print(sb)
-}
-
-fun combi(cnt: Int = 0, begin: Int = 0, depth: Int = 2) {
-    if(cnt == depth) {
-        val (a, b) = select
-        val dist = arr[b]-arr[a]
-        if(dist % 2 == 0) {
-            if(arr.binarySearch(arr[a] + dist / 2) >= 0) same++
-        }
-        return
-    }
-
-    for(i in begin until arr.size) {
-        select.add(i)
-        combi(cnt + 1, i + 1, depth)
-        select.removeLast()
-    }
 }
