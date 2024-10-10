3a424f6a4872a5f2571cb0c225b4b497d53602d9
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-10-10 16:01:57 +0900

    [Silver II] Title: Three Dots, Time: 1196 ms, Memory: 292460 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.kt
+++ 백준/Silver/13423. Three Dots/Three Dots.kt
@@ -1,6 +1,6 @@
-var same = 0
 lateinit var arr: IntArray
 lateinit var select: ArrayList<Int>
+var same = 0
 
 fun main() = java.io.StreamTokenizer(System.`in`.bufferedReader()).run {
     fun i():Int{nextToken();return nval.toInt()};val sb=StringBuilder()
@@ -21,7 +21,7 @@ fun combi(cnt: Int = 0, begin: Int = 0, depth: Int = 2) {
         val (a, b) = select
         val dist = arr[b]-arr[a]
         if(dist % 2 == 0) {
-            if(arr[a] + dist / 2 in arr) same++
+            if(arr.binarySearch(arr[a] + dist / 2) >= 0) same++
         }
         return
     }
