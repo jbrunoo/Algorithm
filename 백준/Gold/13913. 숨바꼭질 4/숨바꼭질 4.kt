341e214e43a07d26caadcfd44f436b9ac36d13d2
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-10-14 20:48:42 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 408 ms, Memory: 83708 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.kt
@@ -0,0 +1,52 @@
+import java.util.*
+
+private lateinit var visited: IntArray
+private lateinit var prev: IntArray
+private lateinit var move: ArrayList<Int>
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val st = StringTokenizer(readLine())
+    val n = st.nextToken().toInt(); val k = st.nextToken().toInt()
+    visited = IntArray(100001)
+    prev = IntArray(100001)
+    move = ArrayList()
+    move.add(k)
+
+    bfs(n, k)
+}
+
+fun bfs(n: Int, k: Int) {
+    val q = ArrayDeque<Int>()
+    q.offer(n)
+    visited[n] = 1
+
+    while(q.isNotEmpty()) {
+        val p = q.poll()
+
+        if(p == k) {
+            val min = visited[p]-1
+            again(0, min, k)
+            println(min)
+            println(move.reversed().joinToString(" "))
+            break
+        }
+
+        for(next in listOf(p - 1, p + 1, p * 2)) {
+            if(next in 0..100000) {
+                if(visited[next] == 0 || visited[next] == visited[p] + 1) {
+                    visited[next] = visited[p] + 1
+                    prev[next] = p
+                       q.offer(next)
+                }
+            }
+        }
+    }
+}
+
+fun again(cnt: Int, min: Int, idx: Int) {
+    if(cnt == min) {
+        return
+    }
+    move.add(prev[idx])
+    again(cnt + 1, min, prev[idx])
+}
