f476a041caca26a8a5312f35641652c4db2231d8
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-10-17 02:36:24 +0900

    [Silver V] Title: 차량 번호판 2, Time: 160 ms, Memory: 21044 KB -BaekjoonHub

--- 백준/Silver/16969. 차량 번호판 2/차량 번호판 2.kt
+++ 백준/Silver/16969. 차량 번호판 2/차량 번호판 2.kt
@@ -1,15 +1,13 @@
-fun main() = print(solve(readln()))
-fun solve(str: String):Long {
+fun main() = with(System.`in`.bufferedReader()) {
+    print(solve(readLine()))
+}
+
+fun solve(str: String): Long {
     var prevChar = ' '
     var ret = 1L
-    for(c in str) {
-        if(prevChar == c) {
-            if(c == 'c') ret *= 25
-            else ret *= 9
-        } else {
-            if(c == 'c') ret *= 26
-            else ret *= 10
-        }
+    for (c in str) {
+        ret *= if (prevChar == c) { if (c == 'c') 25 else 9 }
+        else { if (c == 'c') 26 else 10 }
         ret %= 1000000009
         prevChar = c
     }
