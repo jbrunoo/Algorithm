d044cc1837d22463d531bf7eb22eadef936c3c3b
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-10-16 23:52:07 +0900

    [Bronze V] Title: 개수 세기, Time: 72 ms, Memory: 12272 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.kt
+++ 백준/Bronze/10807. 개수 세기/개수 세기.kt
@@ -1,2 +1,1 @@
-import java.io.StreamTokenizer
-fun main()=StreamTokenizer(System.`in`.bufferedReader()).run{fun i():Int{nextToken();return nval.toInt()};val n=i();val a=IntArray(n){i()};val v=i();print(a.count{it==v})}
+fun main()=java.io.StreamTokenizer(System.`in`.bufferedReader()).run{fun i():Int{nextToken();return nval.toInt()};val a=IntArray(i()){i()};print(a.count{it==i()})}
