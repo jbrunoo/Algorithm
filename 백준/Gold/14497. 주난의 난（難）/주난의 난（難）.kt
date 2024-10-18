1de6e817a654d270bf583345255a4a2ac5e6eb32
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-10-18 17:09:37 +0900

    [Gold IV] Title: 주난의 난(難), Time: 220 ms, Memory: 20060 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.kt
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.kt
@@ -4,15 +4,17 @@ private lateinit var arr: Array<IntArray>
 private lateinit var visited: Array<BooleanArray>
 private var n = 0
 private var m = 0
-private var flag = false
+val dx = intArrayOf(-1, 1, 0, 0)
+val dy = intArrayOf(0, 0, -1, 1)
 
 fun main() = with(System.`in`.bufferedReader()) {
     var st = StringTokenizer(readLine())
     fun i() = st.nextToken().toInt()
-    n = i();m = i()
+    n = i(); m = i()
     st = StringTokenizer(readLine())
-    val x1 = i()-1; val y1 = i()-1;i();i()
+    val x1 = i()-1; val y1 = i()-1; val x2 = i()-1; val y2 = i()-1
     arr = Array(n) { IntArray(m) }
+    visited = Array(n) { BooleanArray(m) }
 
     repeat(n) {
         val row = readLine()
@@ -22,37 +24,35 @@ fun main() = with(System.`in`.bufferedReader()) {
         }
     }
 
+    val q = ArrayDeque<Int>()
+    q.offer(1000 * x1 + y1)
+
     var jump = 0
-    while(true) {
+    while(arr[x2][y2] != 0) {
         jump++
-        visited = Array(n) { BooleanArray(m) }
-        dfs(x1, y1)
-        if(flag) break
+        val tmp = ArrayDeque<Int>()
+
+        while(q.isNotEmpty()) {
+            val x = q.peek() / 1000; val y = q.poll() % 1000
+            visited[x][y] = true
+
+            for(i in 0..3) {
+                val nx = x + dx[i]
+                val ny = y + dy[i]
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny]) continue
+                visited[nx][ny] = true
+                when(arr[nx][ny]) {
+                    0 -> q.offer(1000 * nx + ny)
+                    1, 2 -> {
+                        arr[nx][ny] = 0
+                        tmp.offer(1000 * nx + ny)
+                    }
+                }
+            }
+        }
+        q.addAll(tmp)
     }
 
     print(jump)
-}
-
-val dx = intArrayOf(-1, 1, 0, 0)
-val dy = intArrayOf(0, 0, -1, 1)
-
-fun dfs(x: Int, y: Int) {
-    visited[x][y] = true
-
-    for (i in 0..3) {
-        val nx = x + dx[i]
-        val ny = y + dy[i]
-
-        if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny]) continue
-        visited[nx][ny] = true
-        when(arr[nx][ny]) {
-            0 -> dfs(nx, ny)
-            1 -> arr[nx][ny] = 0
-            2 -> {
-                flag = true
-                return
-            }
-        }
-    }
-
 }
