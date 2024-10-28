2b7107095d285405bac4f2acad0cdf9f5ed37725
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-10-28 23:48:38 +0900

    [Silver I] Title: 주지수, Time: 412 ms, Memory: 31392 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.kt
@@ -0,0 +1,24 @@
+    fun main() = java.io.StreamTokenizer(System.`in`.bufferedReader()).run {
+        fun i(): Int { nextToken();return nval.toInt() }
+        val n = i(); val m = i()
+        val land = Array(n+1) { IntArray(m+1) }
+        repeat(n) { i -> repeat(m) { j -> land[i+1][j+1] = i() } }
+        
+        val dp = Array(n + 1) { IntArray(m + 1) }
+
+        for(i in 1..n) {
+            for(j in 1..m) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + land[i][j]
+            }
+        }
+
+        val sb = StringBuilder()
+        repeat(i()) {
+            val x1 = i(); val y1 = i(); val x2 = i(); val y2 = i()
+            val pop = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]
+
+            sb.append(pop).append("\n")
+        }
+
+        print(sb)
+    }
