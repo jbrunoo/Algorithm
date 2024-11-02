6cffabee850a99c0189fba7c23655fa062ca5c84
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-11-02 21:00:38 +0900

    [Silver II] Title: 꽃길, Time: 200 ms, Memory: 20744 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.kt
+++ 백준/Silver/14620. 꽃길/꽃길.kt
@@ -2,7 +2,6 @@ import kotlin.math.min
 
 private lateinit var arr: Array<IntArray>
 private lateinit var visited: Array<BooleanArray>
-private lateinit var seed: MutableList<Pos>
 var n = 0
 var min = Int.MAX_VALUE
 
@@ -11,43 +10,47 @@ fun main() = java.io.StreamTokenizer(System.`in`.bufferedReader()).run {
     n = i()
     arr = Array(n) { IntArray(n) { i() } }
     visited = Array(n) { BooleanArray(n) }
-    seed = mutableListOf()
 
-    dfs(0,3)
+    dfs(0, 0)
     println(min)
 }
 
-fun dfs(cnt: Int, depth: Int) {
-    if(cnt == depth) {
-        min = min(min, getPrice())
+fun dfs(cnt: Int, price: Int) {
+    if(cnt == 3) {
+        min = min(min, price)
         return
     }
 
     for(i in 1 until n - 1) {
         for(j in 1 until n - 1) {
-            if(visited[i][j] || visited[i+1][j] || visited[i-1][j] || visited[i][j+1] || visited[i][j-1]) continue
-            if(visited[i+1][j+1] || visited[i+1][j-1] || visited[i-1][j+1] || visited[i-1][j-1]) continue
-            if(i != 1 && visited[i - 2][j]) continue
-            if(i != n - 2 && visited[i + 2][j]) continue
-            if(j != 1 && visited[i][j - 2]) continue
-            if(j != n - 2 && visited[i][j + 2]) continue
-
-            seed.add(Pos(i, j))
-            visited[i][j] = true
-            dfs(cnt + 1, depth)
-            seed.removeLast()
-            visited[i][j] = false
+            if(check(i, j)) {
+                visitFlower(i, j)
+                dfs(cnt + 1, price + getPrice(i, j))
+                visitFlower(i, j)
+            }
         }
     }
 }
 
-fun getPrice(): Int {
-    var price = 0
-    for(pos in seed) {
-        val x = pos.x; val y = pos.y
-        price += arr[x][y] + arr[x+1][y] + arr[x][y+1] + arr[x-1][y] + arr[x][y-1]
-    }
-    return price
+fun visitFlower(i: Int, j: Int) {
+        visited[i][j] = !visited[i][j]
+        visited[i+1][j] = !visited[i+1][j]
+        visited[i-1][j] = !visited[i-1][j]
+        visited[i][j+1] = !visited[i][j+1]
+        visited[i][j-1] = !visited[i][j-1]
 }
 
-data class Pos(val x: Int, val y: Int)
+val dx = listOf(0, 0, -1, 1)
+val dy = listOf(-1, 1, 0, 0)
+
+fun check(x: Int, y: Int): Boolean {
+    for(i in 0..3) {
+        val nx = x + dx[i]
+        val ny = y + dy[i]
+
+        if(visited[nx][ny]) return false
+    }
+    return true
+}
+
+fun getPrice(x: Int, y:Int) = arr[x][y] + arr[x+1][y] + arr[x][y+1] + arr[x-1][y] + arr[x][y-1]
