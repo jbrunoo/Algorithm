5bebc6c0082b4c2d5c85394faa2db517e8715245
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-11-09 03:05:50 +0900

    [Silver I] Title: 태권왕, Time: 80 ms, Memory: 12440 KB -BaekjoonHub

--- 백준/Silver/14562. 태권왕/태권왕.kt
+++ 백준/Silver/14562. 태권왕/태권왕.kt
@@ -1,23 +1,17 @@
 import java.util.*
-lateinit var q: ArrayDeque<Score>
 fun main()=java.io.StreamTokenizer(System.`in`.bufferedReader()).run{
     fun i():Int{nextToken();return nval.toInt()}
-    q=ArrayDeque()
     StringBuilder().run{
         repeat(i()){
-            q.clear()
+            val q=ArrayDeque<Score>()
             q.offer(Score(i(),i(),0))
-            appendLine(bfs())}
+            while(q.isNotEmpty()){
+                val(s,t,c)=q.poll()
+                if(s==t){appendLine(c);break}
+                if(s*2<=t+3)q.offer(Score(s*2,t+3,c+1))
+                if(s+1<=t)q.offer(Score(s+1,t,c+1))
+            }}
         print(this)
     }
-}
-fun bfs():Int{
-    while(q.isNotEmpty()){
-        val(s,t,c)=q.poll()
-        if(s==t)return c
-        if(s*2<=t+3)q.offer(Score(s*2,t+3,c + 1))
-        if(s+1<=t)q.offer(Score(s+1,t,c+1))
-    }
-    return 0
 }
 data class Score(val s:Int, val t:Int, val c:Int)
