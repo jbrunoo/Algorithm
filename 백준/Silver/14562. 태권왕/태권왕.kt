0a142d16271fe78b5fad868f6034878b1002a159
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-11-09 02:51:46 +0900

    [Silver I] Title: 태권왕, Time: 84 ms, Memory: 12428 KB -BaekjoonHub

--- 백준/Silver/14562. 태권왕/태권왕.kt
+++ 백준/Silver/14562. 태권왕/태권왕.kt
@@ -1,51 +1,25 @@
+import java.io.StreamTokenizer
 import java.util.*
-
 lateinit var q: ArrayDeque<Score>
-lateinit var temp: ArrayDeque<Score>
-
-fun main() = (System.`in`.bufferedReader()).run {
-    val c = readLine().toInt()
+fun main() = StreamTokenizer(System.`in`.bufferedReader()).run {
+    fun i():Int{nextToken();return nval.toInt()}
     val sb = StringBuilder()
     q = ArrayDeque()
-    temp = ArrayDeque()
-
-    repeat(c) {
-        val st = StringTokenizer(readLine())
-        val s = st.nextToken().toInt();
-        val t = st.nextToken().toInt()
-
+    repeat(i()) {
         q.clear()
-        temp.clear()
-        q.offer(Score(s, t))
-
-        var cnt = 0
-        while(true) {
-            if(bfs()) break
-            q.addAll(temp)
-            temp.clear()
-            cnt++
-        }
-
-        sb.append(cnt).append("\n")
+        q.offer(Score(i(), i(), 0))
+        sb.append(bfs()).append("\n")
     }
-
     print(sb)
 }
-
-fun bfs(): Boolean {
+fun bfs(): Int {
     while (q.isNotEmpty()) {
-        val (x, y) = q.poll()
-        if (x == y) return true
-
-        val nx1 = x * 2
-        val ny1 = y + 3
-        val nx2 = x + 1
-        val ny2 = y
-
-        if(nx1 <= ny1) temp.offer(Score(nx1, ny1))
-        if(nx2 <= ny2) temp.offer(Score(nx2, ny2))
+        val (s, t, c) = q.poll()
+        if (s == t) return c
+        val x = s * 2;val y = t + 3;val z = s + 1
+        if (x <= y) q.offer(Score(x, y, c + 1))
+        if (z <= t) q.offer(Score(z, t, c + 1))
     }
-    return false
+    return 0
 }
-
-data class Score(val s: Int, val t: Int)
+data class Score(val s:Int, val t:Int, val c:Int)
