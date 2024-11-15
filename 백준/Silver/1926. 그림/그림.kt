f779e89ece547c306b769d455b8df4eaff23b719
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2024-11-16 00:46:04 +0900

    [Silver I] Title: 그림, Time: 624 ms, Memory: 57572 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.kt
+++ 백준/Silver/1926. 그림/그림.kt
@@ -1,13 +1,12 @@
+import kotlin.math.max
+
 private lateinit var arr: Array<IntArray>
 private lateinit var visited: Array<BooleanArray>
 var n = 0
 var m = 0
-var cnt = 0
 
 fun main() = java.io.StreamTokenizer(System.`in`.bufferedReader()).run {
-    fun i(): Int {
-        nextToken(); return nval.toInt()
-    }
+    fun i(): Int { nextToken(); return nval.toInt() }
 
     n = i(); m = i()
     arr = Array(n) { IntArray(m) { i() } }
@@ -19,30 +18,28 @@ fun main() = java.io.StreamTokenizer(System.`in`.bufferedReader()).run {
         for (j in 0 until m) {
             if (arr[i][j] == 1 && !visited[i][j]) {
                 ret++
-                dfs(i, j)
-                size = maxOf(size, cnt)
-                cnt = 0
+                size = max(size, dfs(i, j))
             }
         }
     }
 
-    println(ret)
-    print(size)
+    print("$ret\n$size")
 }
 
 val dx = listOf(-1, 1, 0, 0)
 val dy = listOf(0, 0, -1, 1)
 
-fun dfs(x: Int, y: Int) {
-    cnt++
+fun dfs(x: Int, y: Int): Int {
     visited[x][y] = true
+    var cnt = 1
 
     for (i in 0..3) {
         val nx = x + dx[i]
         val ny = y + dy[i]
 
-        if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] || arr[nx][ny] != 1) continue
-        visited[nx][ny] = true
-        dfs(nx, ny)
+        if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] || arr[nx][ny] == 0) continue
+        cnt += dfs(nx, ny)
     }
+
+    return cnt
 }
