82f9b468939fc9b6649358deb8b8ee27a7fa9739
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2025-02-07 18:29:20 +0900

    [Gold IV] Title: 문자열 폭발, Time: 272 ms, Memory: 29452 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.kt
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.kt
@@ -2,15 +2,24 @@ fun main() = with(System.`in`.bufferedReader()) {
     val str = readLine()
     val explode = readLine()
     val len = explode.length
+    val last = explode.last()
     val sb = StringBuilder()
 
-    str.forEach {
-        sb.append(it)
+    for (c in str) {
+        sb.append(c)
 
-        if (sb.length >= len) {
-            if (explode == sb.slice(sb.length - len until sb.length)) {
-                sb.delete(sb.length - len, sb.length)
+        if (sb.length >= len && sb.last() == last) {
+            var bool = true
+
+            for (i in 0 until len) {
+                if (sb[sb.length - 1 - i] == explode[len - 1 - i]) Unit
+                else {
+                    bool = false
+                    break
+                }
             }
+
+            if (bool) sb.delete(sb.length - len, sb.length)
         }
     }
 
