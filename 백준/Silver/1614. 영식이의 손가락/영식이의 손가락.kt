43ef28983c2d01d91db7166d8e05deb9684c43b6
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2025-03-02 23:52:50 +0900

    [Silver III] Title: 영식이의 손가락, Time: 76 ms, Memory: 12228 KB -BaekjoonHub

--- 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.kt
+++ 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.kt
@@ -3,13 +3,16 @@ fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
     var sum = 0L
 
-    repeat(n) {
-        when (f) {
-            1, 5 -> sum += 8
-            2 -> sum += if(it % 2 == 0) 6 else 2
-            4 -> sum += if(it % 2 == 0) 2 else 6
-            3 -> sum += 4
-        }
+    if(n == 0) {
+        print(f - 1)
+        return@with
+    }
+
+    when (f) {
+        1, 5 -> sum = 8L * n
+        2 -> sum = 8L * (n / 2) + if (n % 2 != 0) 6 else 0
+        4 -> sum = 8L * (n / 2) + if (n % 2 != 0) 2 else 0
+        3 -> sum = 4L * n
     }
 
     print(f + sum - 1)
