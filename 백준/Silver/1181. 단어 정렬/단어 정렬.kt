f403410c81e098065cc0c14afb94cdbb5705d91d
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2025-03-18 20:10:31 +0900

    [Silver V] Title: 단어 정렬, Time: 436 ms, Memory: 37820 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.kt
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.kt
@@ -1,19 +1,20 @@
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
-    val pq = java.util.PriorityQueue<String> { o1, o2 ->
-        if (o1.length == o2.length) o1.compareTo(o2)
-        else o1.length.compareTo(o2.length)
-    }
+    val set = mutableSetOf<String>()
 
     repeat(n) {
-        val str = readLine()
-        if (str !in pq) pq.offer(str)
+        set.add(readLine())
     }
 
     val sb = StringBuilder()
 
-    while (pq.isNotEmpty()) {
-        sb.append(pq.poll()).append("\n")
+    val sorted = set.sortedWith { o1, o2 ->
+        if (o1.length == o2.length) o1.compareTo(o2)
+        else o1.length.compareTo(o2.length)
+    }
+
+    for(str in sorted) {
+        sb.append(str).append("\n")
     }
 
     print(sb)
