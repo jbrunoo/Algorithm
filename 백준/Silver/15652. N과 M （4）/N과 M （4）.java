ac078b9da44eb1f4f3e00a7d07e69478de4282c8
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2025-08-05 09:33:04 +0900

    [Silver III] Title: N과 M (4), Time: 208 ms, Memory: 19324 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,32 +1,32 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	static StringBuilder sb = new StringBuilder();
-	static ArrayList<Integer> li = new ArrayList<>();
+	static int n, m;
+	static int[] arr;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);	
-		int n = sc.nextInt();
-		int m = sc.nextInt();
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[m];
 		
-		permutation(n, m, 1);
+		permutation(1, 0);
 		System.out.println(sb);
 	}
 	
-	private static void permutation(int n, int m, int start) {
-		if (m == 0) {
-			for (int i : li) {
-				sb.append(i).append(" ");
+	private static void permutation(int start, int depth) {
+		if (m == depth) {
+			for (int i : arr) {
+				sb.append(i).append(" ");	
 			}
 			sb.append("\n");
 			return;
 		}
 		
 		for (int i = start; i <= n; i++) {
-			li.add(i);
-			permutation(n, m - 1, i);
-			li.remove(li.size() - 1);
+			arr[depth] = i;
+			permutation(i, depth + 1);
 		}
 	}
 }
