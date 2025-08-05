5872eb3fc8c0a15fb122ad39ffaa5ee3fe88bc36
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2025-08-05 09:35:53 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 17180 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	static StringBuilder sb = new StringBuilder();
 	static int n, m;
 	static int[] arr;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);	
-		n = sc.nextInt();
-		m = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 		arr = new int[m];
 		
 		permutation(1, 0);
