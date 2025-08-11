6efc05388068491247a2042b42641742e922e02d
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2025-08-11 16:53:06 +0900

    [Silver I] Title: 절댓값 힙, Time: 468 ms, Memory: 26728 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,37 +1,37 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		Queue<Integer> q = new PriorityQueue<>(new Comparator<Integer>() {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(in.readLine());
+
+		int N = Integer.parseInt(in.readLine());
+
+		int num;
+
+		Queue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
 
 			@Override
 			public int compare(Integer o1, Integer o2) {
-				int v = Math.abs(o1) - Math.abs(o2);
-				if (v == 0) return o1 - o2;
-				else return v;	
+				if(Math.abs(o1) == Math.abs(o2)) return o1-o2;
+				return Math.abs(o1) - Math.abs(o2);
 			}
 		});
-		
-		for (int i = 0; i < n; i++) {
-			int x = Integer.parseInt(br.readLine());
-			
-			if (x != 0) q.offer(x);
-			else {
-				if (q.isEmpty()) sb.append(0).append("\n");
-				else sb.append(q.poll()).append("\n");
+		for (int i = 0; i < N; i++) {
+			num = Integer.parseInt(in.readLine());
+			if (num != 0) {
+				queue.offer(num);
+			}
+			else if(queue.isEmpty()) {
+				System.out.println(0);
+			} else {
+				System.out.println(queue.poll());
 			}
 		}
-		
-		System.out.println(sb);
 	}
 }
