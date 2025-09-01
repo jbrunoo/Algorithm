4d0eee179a2cacea08f52d0bd76d7910cf0c1cac
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2025-09-01 17:10:39 +0900

    [Silver II] Title: 색종이 만들기, Time: 132 ms, Memory: 15704 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,48 +1,46 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] board;
+	static int N;
+	static int[][] paper;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		board = new int[N][N];
+
+		N = Integer.parseInt(br.readLine());
+		paper = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+				paper[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		int blueCount = recursion(0, 0, N, 1);
-		int whiteCount = recursion(0, 0, N, 0);
-		System.out.println(whiteCount + "\n" + blueCount);
+		System.out.println(count(0, 0, N, 0));
+		System.out.println(count(0, 0, N, 1));
 	}
 
-	public static int recursion(int r, int c, int k, int color) {
+	static int count(int i, int j, int n, int color) {
 		boolean uniform = true;
-		for (int i = r; i < r + k; i++) {
-			for (int j = c; j < c + k; j++) {
-				if (board[i][j] != color) {
+		loop: for (int a = i; a < i + n; a++) {
+			for (int b = j; b < j + n; b++) {
+				if (paper[a][b] != color) {
 					uniform = false;
-					break;
+					break loop;
 				}
 			}
-			if (!uniform)
-				break;
 		}
 		if (!uniform) {
-			int count = 0;
-			count += recursion(r, c, k / 2, color);
-			count += recursion(r, c + k / 2, k / 2, color);
-			count += recursion(r + k / 2, c, k / 2, color);
-			count += recursion(r + k / 2, c + k / 2, k / 2, color);
-			return count;
+			int c = 0;
+			c += count(i, j, n / 2, color);
+			c += count(i + n / 2, j, n / 2, color);
+			c += count(i, j + n / 2, n / 2, color);
+			c += count(i + n / 2, j + n / 2, n / 2, color);
+			return c;
 		} else {
-			return (board[r][c] == color) ? 1 : 0;
+			return paper[i][j] == color ? 1 : 0;
 		}
 	}
 }
