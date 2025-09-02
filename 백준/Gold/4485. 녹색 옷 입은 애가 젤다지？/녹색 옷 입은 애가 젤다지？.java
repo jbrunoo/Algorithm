943ae47b43059e9a883d13622fcd7a773fc847f2
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2025-09-02 17:47:48 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 148 ms, Memory: 17396 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -2,118 +2,88 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	private static class Pos implements Comparable<Pos> {
-		int pos, weight;
-		Pos link;
-		
-		public Pos(int pos, int weight, Pos link) {
-			this.pos = pos;
-			this.weight = weight;
-			this.link = link;
-		}
 
-		@Override
-		public int compareTo(Pos o) {
-			return this.weight - o.weight;
-		}
-	}
-	
 	private static int n;
-	
-	private static int[][] board;
-	
-	private static Pos[] cave;
-	private static boolean[] visited;
-	private static int[] distance;
-	
-	private static int[] di = { -1, 1, 0, 0 };
-	private static int[] dj = { 0, 0, -1, 1 };
-	
+	private static int[][] cave;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
-		int t = 0;
+		int cnt = 1;
 		
 		while (true) {
 			n = Integer.parseInt(br.readLine());
+			cave = new int[n][n];
 			if (n == 0) break;
 			
-			board = new int[n][n];
-			cave = new Pos[n * n];
-			visited = new boolean[n * n];
-			distance = new int[n * n];
-			Arrays.fill(distance, Integer.MAX_VALUE);
-						
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
 				
 				for (int j = 0; j < n; j++) {
-					board[i][j] = Integer.parseInt(st.nextToken());
+					cave[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			int cnt = 0;
-			
-			for (int idx = 0; idx < n * n; idx++) {
-				int i = idx / n;
-				int j = idx % n;
-				
-				for (int k = 0; k < 4; k++) {
-					int ni = i + di[k];
-					int nj = j + dj[k];
-					
-					if (ni < 0 || nj < 0 || ni >= n || nj >= n) continue;
-					int neighborIdx = ni * n + nj;
-					cave[cnt] = new Pos(neighborIdx, board[ni][nj], cave[cnt]);	
-//					System.out.println("cnt" + cnt + " pos " + neighborIdx);
-				}
-				cnt++;
-			}
-//			cntt = 0;
-			dijkstra();
-//			System.out.println(cntt);
-			
-			sb.append("Problem ").append(++t).append(": ").append(distance[n * n - 1]).append("\n");
+			sb.append("Problem").append(" ").append(cnt).append(":").append(" ").append(dijkstra()).append("\n");
+			cnt++;
 		}
 		
 		System.out.println(sb);
 	}
-
-//	private static int cntt;
 	
-	private static void dijkstra() {
-		distance[0] = board[0][0];
+	private static int dijkstra() {
+		PriorityQueue<Pos> pq = new PriorityQueue<>();
+		pq.offer(new Pos(0, 0, cave[0][0]));
+		int[][] dist = new int[n][n];
 		
-		Queue<Pos> pq = new PriorityQueue<>();
-		pq.offer(new Pos(0, distance[0], null));
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(dist[i], Integer.MAX_VALUE);	
+		}
 		
+		int[] dx = { -1, 1, 0, 0 };
+		int[] dy = { 0, 0, -1, 1 };
 		
 		while (!pq.isEmpty()) {
-			Pos current = pq.poll();
+			Pos p = pq.poll();
+			int x = p.x;
+			int y = p.y;
+			int k = p.k;
 			
-			if (visited[current.pos]) continue;
-			visited[current.pos] = true;
+			if (k > dist[x][y]) continue;
 			
-			if (current.pos == n * n - 1) return;
+			if (x == n - 1 && y == n - 1) return k;
 			
-//			cntt++;
-			
-			for (Pos next = cave[current.pos]; next != null; next = next.link) {
-				if (!visited[next.pos] && distance[next.pos] > current.weight + next.weight) {
-					distance[next.pos] = current.weight + next.weight;
-					pq.offer(new Pos(next.pos, distance[next.pos], null));
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i]; 
+				int ny = y + dy[i]; 
+				
+				if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+				if (k + cave[nx][ny] < dist[nx][ny]) {
+					dist[nx][ny] = k + cave[nx][ny];
+					pq.offer(new Pos(nx, ny, dist[nx][ny]));
 				}
 			}
 		}
 		
-		return;
+		return -1;
+	}
+	
+	private static class Pos implements Comparable<Pos> {
+		int x, y, k;
+		
+		public Pos(int x, int y, int k) {
+			this.x = x;
+			this.y = y;
+			this.k = k;
+		}
+
+		@Override
+		public int compareTo(Pos o) {
+			return this.k - o.k;
+		}
 	}
 }
