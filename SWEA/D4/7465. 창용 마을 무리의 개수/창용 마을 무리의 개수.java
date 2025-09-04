c3f5b7033d7a1b3ee2ebb95ae08d61e125a404fd
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2025-09-04 14:17:23 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 100 ms, Memory: 28,672 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,70 +1,64 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
-class Solution {    
+public class Solution {
 	
-	private static int n;
-	private static int[] parent;
-	
+	private static int n, m;
+	private static int[] parents;
+		
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
+
 		int T = Integer.parseInt(br.readLine());
 		
 		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
-			makeSet();
+			m = Integer.parseInt(st.nextToken());
+			parents = new int[n];
+		
+			for (int i = 0; i < n; i++) {
+				parents[i] = i;
+			}
 			
-			for (int i = 0; i < m; i++) {
+			for (int i = 0 ; i < m; i++) {
 				st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 				
-				union(from, to);
+				union(from - 1, to - 1);
 			}
 			
 			HashSet<Integer> set = new HashSet<>();
 			
-			for (int i = 1; i <= n; i++) {
+			for (int i : parents) {
 				set.add(findSet(i));
 			}
 			
 			sb.append("#").append(t).append(" ").append(set.size()).append("\n");
 		}
-		
-		System.out.println(sb);
-	}
-	
-	private static void makeSet() {
-		parent = new int[n + 1];
-		
-		for (int i = 1; i <= n; i++) {
-			parent[i] = i;
-		}
+
+		System.out.println(
+				sb);
 	}
 	
 	private static int findSet(int a) {
-		if (parent[a] == a) return a;
+		if (parents[a] == a) return a;
 		
-		return parent[a] = findSet(parent[a]);
+		return parents[a] = findSet(parents[a]);
 	}
 	
 	private static boolean union(int a, int b) {
-		int rootA = findSet(a);
-		int rootB = findSet(b);
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
 		
-		if (rootA == rootB) {
-			return false;
-		}
+		if (aRoot == bRoot) return false;
 		
-		parent[rootB] = rootA;
+		parents[bRoot] = aRoot;
 		return true;
 	}
 }
