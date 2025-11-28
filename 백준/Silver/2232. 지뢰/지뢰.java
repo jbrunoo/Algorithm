f5a8f80898e1d84ee4822ff57a80d922ed253de1
Harim Jeong <125545555+jbrunoo@users.noreply.github.com>
2025-11-28 10:36:04 +0900

    [Silver II] Title: 지뢰, Time: 1004 ms, Memory: 76848 KB -BaekjoonHub

--- 백준/Silver/2232. 지뢰/지뢰.java
+++ 백준/Silver/2232. 지뢰/지뢰.java
@@ -1,88 +1,33 @@
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
-	
-	static int n;
-	static int[] arr;
-	
+public class Main {	
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		arr = new int[n];
+		int n = sc.nextInt();
+		int[] arr = new int[n];
 		
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
 		
-		ArrayList<Integer> mine = new ArrayList<>();
-		
-		while (true) {
-			if (isEmpty()) break;
-			int startIdx = findMaxIdx();
-			mine.add(startIdx + 1);
-			bombNeighbor(startIdx);
+		if (arr.length == 1) {
+			System.out.println(1);
+			return;
 		}
-		
-		mine.sort(null);
-		
-		for (int m : mine) {
-			System.out.println(m);
-		}
-	}
-	
-	private static boolean isEmpty() {
-		for (int a : arr) {
-			if (a != 0) return false;
-		}
-		return true;
-	}
-	
-	private static int findMaxIdx() {
-		int max = 0;
-		int maxIdx = 0;
 		
 		for (int i = 0; i < arr.length; i++) {
-			if (max < arr[i]) {
-				max = arr[i];
-				maxIdx = i;
-			}
-		}
-		
-		return maxIdx;
-	}
-	
-	private static void bombNeighbor(int startIdx) {
-		int leftBombSize = arr[startIdx];
-		int rightBombSize = arr[startIdx];
-		arr[startIdx] = 0;
-		int left = startIdx;
-		int right = startIdx;
-		
-		for (int i = 0; i < n; i++) {
-			left--;
-			right++;
-			
-			if (left >= 0) {
-				if (arr[left] != 0 && arr[left] < leftBombSize) {
-					leftBombSize = arr[left];
-					arr[left] = 0;
-				} else {
-					left = -1;
-				}
+			if (i == 0) {
+				if (arr[i] >= arr[i + 1]) System.out.println(i + 1);
 			}
 			
-			if (right < n) {
-				if (arr[right] != 0 && arr[right] < rightBombSize) {
-					rightBombSize = arr[right];
-					arr[right] = 0;					
-				} else {
-					right = n;
-				}
+			if (i > 0 && i < n - 1) {
+				if (arr[i - 1] <= arr[i] && arr[i] >= arr[i + 1]) System.out.println(i + 1);				
 			}
 			
-			if (left < 0 && right >= n) break;
+			if (i == n - 1) {
+				if (arr[i - 1] <= arr[i]) System.out.println(i + 1);
+			}
 		}
 	}
 }
